6d178272f46d5cf2e065baaa25be0fdb62303b35
Louis Diaz <le.diaz@icloud.com>
2022-02-09 21:54:46 -0600

    minor edit

--- lua/louis/nvim-treesitter.lua
+++ lua/louis/nvim-treesitter.lua
@@ -19,17 +19,17 @@ nvim_treesitter_configs.setup {
     ignore_install = { "javascript" },
 
     highlight = {
-    -- `false` will disable the whole extension
-    enable = true,
+        -- `false` will disable the whole extension
+        enable = true,
 
-    -- list of language that will be disabled
-    disable = { "c", "rust" },
+        -- list of language that will be disabled
+        disable = { "c", "rust" },
 
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
     },
 
 }
